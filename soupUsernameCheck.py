832935c5087ca55baed4497e3fe065fc323e76aa
BuildTools <unconfigured@null.spigotmc.org>
2020-06-21 20:33:02 -0400

    redid keywords to be case-insensitive.
    next() instead of any() is now used to be more efficient
    (now the checkWebsite function will stop upon finding one match rather than checking every single keyword)
    
    error code handling is now used to be more efficient and skip rate limited requests.
    
    added then removed a retry function when a rate limited request is used - didn't want to spam sites.

--- soupUsernameCheck.py
+++ soupUsernameCheck.py
@@ -2,17 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 
 def checkWebsite(username, website):
-    keywords = ('not found', '''doesn't exist''', 'not exist', '''isn't available''',
-                'Not Found', '''Doesn't Exist''', 'Not Exist', '''Isn't Available''',
-                'Not found', '''Doesn't exist''', 'Not exist', '''Isn't available''')
 
+    keywords = ('not found', '''n't found''', '''doesn't exist''', 'not exist', '''isn't available''', 'not available', 'unavailable')
     result = requests.get(website + username)
-    src = result.content
-    soup = BeautifulSoup(src,'lxml')
 
-    str_soup = str(soup.encode("utf-8"))
+    code = result.status_code
+    if code == 404:
+        print(username + " is available on " + website)
+        return
+    elif code == 429:
+        print("rate limited on " + website + "! try again later.")
+        return
 
-    if any(x in str_soup for x in keywords):
+    soup = BeautifulSoup(result.content,'lxml')
+    str_soup = str(soup.encode("utf-8")).lower()
+
+    match = next((x for x in keywords if x in str_soup), False)
+    if match != False:
         print(username + " is available on " + website)
     else:
         print(username + " is unavailable on " + website)
@@ -22,8 +28,8 @@ username = input(": ")
 websites = ("https://github.com/",
             "https://old.reddit.com/user/",
             "https://youtube.com/",
-            "https://twitter.com/",
-            "https://instagram.com/")
+            "https://instagram.com/",
+            "https://open.spotify.com/user/")
 
 for i in websites:
     checkWebsite(username, i)
