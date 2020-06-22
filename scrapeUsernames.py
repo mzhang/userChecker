8e64008159d2d487e2e822f1c9c667df71cc6e15
BuildTools <unconfigured@null.spigotmc.org>
2020-06-21 22:38:22 -0400

    did a LOT of things!!
    i made scrapeUsernames output into the actual usernames.txt file.
    now you can just run scrapeUsernames.py then soupUsernameCheck.py to check a list of 25 usernames!
    
    i made it so that any file opened will be eventually closed. so my code can look a little less awful.
    
    i'll work on a .bat script to run both .py files and add some stuff to the README next!

--- scrapeUsernames.py
+++ scrapeUsernames.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
-driver = webdriver.Firefox()
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Firefox(options=options)
+
 driver.get("https://www.namegenerator.biz/fantasy-name-generator.php")
 generateButton = driver.find_element_by_xpath('''//input[@value="Generate Fantasy Names"]''')
 
@@ -9,8 +13,14 @@ for i in range(25):
 
 generatedList = driver.find_element_by_xpath('''//ul[@id="textbox"]''')
 items = generatedList.find_elements_by_tag_name("li")
-for item in items:
-    text = item.text
-    print(text)
+
+with open("usernames.txt", 'w') as f:
+    output = []
+
+    for item in items:
+        output = item.text
+        f.write(output + "\n")
+
+    f.close()
 
 driver.quit()
